cabec8912981b4a76ff34806eda04fd27bc40188
Roman <roman.lemontagner@gmail.com>
2025-04-22 15:39:52 +0200

    fix test

--- tests/reader_80col_test.rs
+++ tests/reader_80col_test.rs
@@ -13,7 +13,7 @@ fn test_80col_reader() {
     let obs_33803 = traj_set.get(&ObjectNumber::String("33803".into())).unwrap();
     assert_eq!(traj_set.len(), 1);
     assert_eq!(obs_33803.len(), 1812);
-    assert_eq!(obs_33803[0].time, 43041.93878);
+    assert_eq!(obs_33803[0].time, 43041.93932611111);
     assert_eq!(obs_33803[0].ra, 359.7403333333333);
     assert_eq!(obs_33803[0].dec, -0.5039444444444444);
     assert_eq!(
@@ -26,7 +26,7 @@ fn test_80col_reader() {
     assert_eq!(traj_set.len(), 2);
     let obs_8467 = traj_set.get(&ObjectNumber::String("8467".into())).unwrap();
     assert_eq!(obs_8467.len(), 3748);
-    assert_eq!(obs_8467[0].time, 43785.35799);
+    assert_eq!(obs_8467[0].time, 43785.35855925926);
     assert_eq!(obs_8467[0].ra, 14.62025);
     assert_eq!(obs_8467[0].dec, 9.987777777777778);
     assert_eq!(
@@ -41,7 +41,7 @@ fn test_80col_reader() {
         .get(&ObjectNumber::String("K25D50B".into()))
         .unwrap();
     assert_eq!(obs_k25.len(), 20);
-    assert_eq!(obs_k25[0].time, 60732.280490000005);
+    assert_eq!(obs_k25[0].time, 60732.28129074074);
     assert_eq!(obs_k25[0].ra, 154.65650833333333);
     assert_eq!(obs_k25[0].dec, 29.973188888888888);
     assert_eq!(
