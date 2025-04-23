a3f84de3eaa9029ed7f00e0d825cb50dea1bf2dc
Roman <roman.lemontagner@gmail.com>
2025-04-23 14:58:07 +0200

    add two body solver for equinoctial element

--- src/equinoctial_element.rs
+++ src/equinoctial_element.rs
@@ -1,4 +1,15 @@
-use crate::{kepler::principal_angle, keplerian_element::KeplerianElements};
+use core::f64;
+use std::f64::consts::PI;
+
+use nalgebra::Vector3;
+use roots::{find_root_newton_raphson, SimpleConvergency};
+
+use crate::{
+    constants::{DPI, GAUSS_GRAV_SQUARED},
+    kepler::principal_angle,
+    keplerian_element::KeplerianElements,
+    outfit_errors::OutfitError,
+};
 
 /// Equinoctial orbital elements.
 /// Units:
@@ -15,6 +26,128 @@ pub struct EquinoctialElements {
     pub tan_half_incl_sin_node: f64, // p = tan(i/2) * sin(Ω)
     pub tan_half_incl_cos_node: f64, // q = tan(i/2) * cos(Ω)
     pub mean_longitude: f64,         // λ = Ω + ω + M
+}
+
+impl EquinoctialElements {
+    fn solve_kepler_equation(
+        &self,
+        mean_longitude_t1: f64,
+        longitude_of_periastre: f64,
+    ) -> Result<f64, OutfitError> {
+        // Fonction R(F) = F - k·sin(F) + h·cos(F) - lambda
+        let f = |fval: f64| -> f64 {
+            fval - self.eccentricity_cos_lon * fval.sin() + self.eccentricity_sin_lon * fval.cos()
+                - mean_longitude_t1
+        };
+
+        // Dérivée R'(F)
+        let df = |fval: f64| -> f64 {
+            1.0 - self.eccentricity_cos_lon * fval.cos() - self.eccentricity_sin_lon * fval.sin()
+        };
+
+        // Point de départ initial (comme en Fortran : pig + pol)
+        let x0 = PI + longitude_of_periastre;
+
+        // Critère de convergence similaire à eps=1e-14
+        let mut tol = SimpleConvergency {
+            eps: f64::EPSILON * 1e2, // ~2e-14
+            max_iter: 25,
+        };
+
+        // Appel à la méthode de Newton-Raphson
+        Ok(find_root_newton_raphson(x0, &f, &df, &mut tol)?)
+    }
+
+    fn compute_cartesian_position_and_velocity(
+        &self,
+        mean_motion: f64,
+        eccentric_anomaly: f64,
+        eccentricity_pow2: f64,
+    ) -> (Vector3<f64>, Vector3<f64>) {
+        let beta = 1. / (1. + (1. - eccentricity_pow2).sqrt());
+
+        let beta_ecc_term = beta * self.eccentricity_sin_lon * self.eccentricity_cos_lon;
+
+        let sin_ecc_anom = eccentric_anomaly.sin();
+        let cos_ecc_anom = eccentric_anomaly.cos();
+
+        let xe = self.semi_major_axis
+            * ((1. - beta * self.eccentricity_sin_lon.powi(2)) * cos_ecc_anom
+                + beta_ecc_term * sin_ecc_anom
+                - self.eccentricity_cos_lon);
+
+        let ye = self.semi_major_axis
+            * ((1. - beta * self.eccentricity_cos_lon.powi(2)) * sin_ecc_anom
+                + beta_ecc_term * cos_ecc_anom
+                - self.eccentricity_sin_lon);
+
+        let u = 1. + self.tan_half_incl_sin_node.powi(2) + self.tan_half_incl_cos_node.powi(2);
+        let inv_u = 1.0 / u;
+
+        let common_component =
+            2. * self.tan_half_incl_sin_node * self.tan_half_incl_cos_node * inv_u;
+
+        let f_vector = Vector3::new(
+            (1. - self.tan_half_incl_sin_node.powi(2) + self.tan_half_incl_cos_node.powi(2))
+                * inv_u,
+            common_component,
+            -2. * self.tan_half_incl_sin_node * inv_u,
+        );
+
+        let g_vector = Vector3::new(
+            common_component,
+            (1. + self.tan_half_incl_sin_node.powi(2) - self.tan_half_incl_cos_node.powi(2))
+                * inv_u,
+            2. * self.tan_half_incl_cos_node * inv_u,
+        );
+
+        let cartesian_position = xe * f_vector + ye * g_vector;
+
+        let v_const = mean_motion * self.semi_major_axis.powi(2) / (xe.powi(2) + ye.powi(2)).sqrt();
+
+        let v_xe = v_const
+            * (beta_ecc_term * cos_ecc_anom
+                - (1. - beta * self.eccentricity_sin_lon.powi(2)) * sin_ecc_anom);
+        let v_ye = v_const
+            * ((1. - beta * self.eccentricity_cos_lon.powi(2)) * cos_ecc_anom
+                - beta_ecc_term * sin_ecc_anom);
+        let cartesian_velocity = v_xe * f_vector + v_ye * g_vector;
+
+        (cartesian_position, cartesian_velocity)
+    }
+
+    pub(crate) fn solve_two_body_problem(
+        &self,
+        t0: f64,
+        t1: f64,
+    ) -> Result<(Vector3<f64>, Vector3<f64>), OutfitError> {
+        let mean_motion = (GAUSS_GRAV_SQUARED / self.semi_major_axis.powi(3)).sqrt();
+        let mut mean_longitude_t1 = self.mean_longitude + mean_motion * (t1 - t0);
+
+        let eccentricity_pow2 =
+            self.eccentricity_sin_lon.powi(2) + self.eccentricity_cos_lon.powi(2);
+        let epsilon = f64::EPSILON * 1e2;
+
+        let mut longitude_of_periastre = 0.0;
+        if eccentricity_pow2 > epsilon {
+            longitude_of_periastre =
+                principal_angle(self.eccentricity_sin_lon.atan2(self.eccentricity_cos_lon));
+        }
+
+        mean_longitude_t1 = principal_angle(mean_longitude_t1);
+        if mean_longitude_t1 < longitude_of_periastre {
+            mean_longitude_t1 += DPI;
+        }
+
+        let eccentric_anomaly =
+            self.solve_kepler_equation(mean_longitude_t1, longitude_of_periastre)?;
+
+        Ok(self.compute_cartesian_position_and_velocity(
+            mean_motion,
+            eccentric_anomaly,
+            eccentricity_pow2,
+        ))
+    }
 }
 
 impl From<EquinoctialElements> for KeplerianElements {
@@ -95,6 +228,54 @@ mod test_equinoctial_element {
                 periapsis_argument: 1.2451951388,
                 mean_anomaly: 0.4405458902000001,
             }
+        );
+    }
+
+    #[test]
+    fn test_kepler_equation() {
+        let equ = EquinoctialElements {
+            reference_epoch: 0.0,
+            semi_major_axis: 1.8017360713154256,
+            eccentricity_sin_lon: 0.26937368090922720,
+            eccentricity_cos_lon: 8.8564152600135601E-002,
+            tan_half_incl_sin_node: 8.0899701663963020E-004,
+            tan_half_incl_cos_node: 0.10168201109730375,
+            mean_longitude: 1.6936970079414786,
+        };
+
+        let result = equ.solve_kepler_equation(1.8432075709935847, 1.2531511177826073);
+
+        assert_eq!(
+            result.unwrap(),
+            2.0450042417470673,
+            "Kepler equation solution does not match expected value"
+        );
+    }
+
+    #[test]
+    fn test_two_body_problem() {
+        let equ = EquinoctialElements {
+            reference_epoch: 0.0,
+            semi_major_axis: 1.8017360713154256,
+            eccentricity_sin_lon: 0.26937368090922720,
+            eccentricity_cos_lon: 8.8564152600135601E-002,
+            tan_half_incl_sin_node: 8.0899701663963020E-004,
+            tan_half_incl_cos_node: 0.10168201109730375,
+            mean_longitude: 1.6936970079414786,
+        };
+
+        let (pos, vel) = equ.solve_two_body_problem(0., 21.019733018845727).unwrap();
+        assert_eq!(
+            pos,
+            Vector3::new(-0.9321264203108841, 1.0784562905421133, 0.22313456997634373)
+        );
+        assert_eq!(
+            vel,
+            Vector3::new(
+                -0.013800441828595238,
+                -0.007301622877053736,
+                -0.001477839051396935
+            )
         );
     }
 }
--- src/outfit_errors.rs
+++ src/outfit_errors.rs
@@ -4,23 +4,35 @@ use thiserror::Error;
 pub enum OutfitError {
     #[error("Invalid JPL string format: {0}")]
     InvalidJPLStringFormat(String),
+
     #[error("Invalid JPL ephemeris file source: {0}")]
     InvalidJPLEphemFileSource(String),
+
     #[error("Invalid JPL ephemeris file version: {0}")]
     InvalidJPLEphemFileVersion(String),
+
     #[error("Invalid URL: {0}")]
     InvalidUrl(String),
+
     #[error("HTTP ureq error: {0}")]
     UreqHttpError(#[from] ureq::Error),
+
     #[error("Unable to perform file operation: {0}")]
     IoError(#[from] std::io::Error),
+
     #[cfg(feature = "jpl-download")]
     #[error("HTTP reqwest error: {0}")]
     ReqwestError(#[from] reqwest::Error),
+
     #[error("Base dir creation error for JPL ephemeris file: {0}")]
     UnableToCreateBaseDir(String),
+
     #[error("UTF-8 Path error: {0}")]
     Utf8PathError(String),
+
     #[error("JPL File not found at: {0}")]
     JPLFileNotFound(String),
+
+    #[error("ROOTS finding error: {0}")]
+    RootFindingError(#[from] roots::SearchError),
 }
