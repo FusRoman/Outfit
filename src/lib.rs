0ea061861191681cb5f8ad3939424190340e3e94
Roman <roman.lemontagner@gmail.com>
2025-06-20 17:25:17 +0200

    finished to work on the rms computation

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,6 @@ pub(crate) mod unit_test_global {
 
             let path_file = Utf8Path::new("tests/data/2015AB.obs");
             let traj_set = TrajectorySet::new_from_80col(&mut env, &path_file);
-            dbg!(traj_set.keys());
             (env, traj_set)
         });
 
